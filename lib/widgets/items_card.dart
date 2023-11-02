4fce6015208fb67a5c71c74a50d8d9c0fa14422b
Aditya Singh <as1671109@gmail.com>
2023-11-02 16:57:26 +0530

    Third Commit

--- lib/widgets/items_card.dart
+++ lib/widgets/items_card.dart
@@ -11,35 +11,32 @@ class ItemsCard extends StatelessWidget {
       child: Card(
         color: Colors.transparent, // Set the background color to transparent
         elevation: 0,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Container(
-                width: 120,
-                height: 120,
-                child: Image.asset('assets/images/item1.png'),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Diya (Medium)",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  FiveStar(),
-                  Text(
-                    "₹2",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Add To Cart"),
-                  )
-                ],
-              ),
-            ],
-          ),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 120,
+              height: 120,
+              child: Image.asset('assets/images/item1.png'),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Diya (Medium)",
+                  style: TextStyle(fontSize: 18),
+                ),
+                FiveStar(),
+                Text(
+                  "₹2",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Add To Cart"),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
